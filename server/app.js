5482c9d6860ae6ee8d8e05105a355c573fe82f35
Phi Nguyen <pnguy55@gmail.com>
2020-02-18 11:48:35 -0600

    Add elipse

--- server/app.js
+++ server/app.js
@@ -41,6 +41,12 @@ mongoose.connection.on("error", err => {
   console.log(`DB connection error: ${err.message}`);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.prepare().then(() => {
   const server = express();
 

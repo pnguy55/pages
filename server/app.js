ddaafce12b02e9419e653c96ee1fdce7da3a757a
Phi Nguyen <pnguy55@gmail.com>
2020-02-24 10:27:42 -0600

    Add elipse

--- server/app.js
+++ server/app.js
@@ -41,14 +41,14 @@ mongoose.connection.on("error", err => {
   console.log(`DB connection error: ${err.message}`);
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.prepare().then(() => {
   const server = express();
+
+  server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
   if (!dev) {
     /* Helmet helps secure our app by setting various HTTP headers */
@@ -71,6 +71,7 @@ app.prepare().then(() => {
     handle(req, res);
   });
 
+  
   const MongoStore = mongoSessionStore(session);
   const sessionConfig = {
     name: "next-connect.sid",
